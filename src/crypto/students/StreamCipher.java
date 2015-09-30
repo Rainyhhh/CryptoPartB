81b9a3fe3616d1211ca19d671f122abb472da45a
Yuqing Han <yuqingh@student.unimelb.edu.au>
2015-09-30 14:15:20 +1000

    a version can run

--- src/crypto/students/StreamCipher.java
+++ src/crypto/students/StreamCipher.java
@@ -22,6 +22,8 @@ public class StreamCipher {
 			BigInteger q) {
 		this.key = share; // shared key from DH
 		this.prime = prime; // DH prime modulus
+//		p1 = p;
+//		p2 = q;
 		this.p1 = Supplementary.deriveSuppementaryKey(share, p);
 		this.p2 = Supplementary.deriveSuppementaryKey(share, q);
 		this.r_i = Supplementary.parityWordChecksum(this.key); // shift register
@@ -32,7 +34,7 @@ public class StreamCipher {
 	 */
 	public void updateShiftRegister() {
 		// log.error("You must implement this function!");
-		r_i = p1.multiply(r_i).add(p2).mod(this.prime);
+		this.r_i = (p1.multiply(r_i).add(p2)).mod(this.prime);
 	}
 
 	/***
@@ -55,7 +57,7 @@ public class StreamCipher {
 	private byte msb(BigInteger value, int n) {
 		// log.error("You must implement this function!");
 		//byte[] msg = value.toByteArray();
-		if(value.bitLength() <= 8) {
+		if(value.compareTo(new BigInteger("2").pow(n).subtract(BigInteger.ONE)) < 0) {
 			return value.byteValue();
 		}
 		BigInteger msg = value.shiftRight(value.bitLength() - n);
@@ -71,8 +73,9 @@ public class StreamCipher {
 	 */
 	private byte[] _crypt(byte[] msg) {
 		// log.error("You must implement this function!");
-		for (int i = 0; i < msg.length; i++) {			
-			msg[i] = (byte) (msg[i] ^ (msb(((p1.multiply(r_i)).add(p2)).mod(this.prime), 8)));
+		for (int i = 0; i < msg.length; i++) {	
+			
+			msg[i] = (byte) (msg[i] ^ msb(this.r_i, 8));
 			updateShiftRegister();
 		}
 		return msg;
@@ -111,10 +114,9 @@ public class StreamCipher {
 		return result;
 	}
 	
-	public static void main(String []args) {
-		StreamCipher s = new StreamCipher(new BigInteger("4"), new BigInteger("11"), new BigInteger("3"), new BigInteger("5"));
-		//System.out.println(msb(new BigInteger("13892949480140891204"), 8));
-		//System.out.println(s.encrypt(""));
-		//System.out.println(Supplementary.parityWordChecksum(new BigInteger("11380312415897726212538720767584623938377542218843650885786488543557849920563944820657401556147072220807050423844611527817088743264179887246035449031879964033048917437051768727745163996083995699396309860629605332639267450328379289961205789359923142431676348109877819086396004913235006262427231898532203764657706261780748597526471127787542155628754978941021278802504747939847153450812209928520258539639347968927907337576400038705453704498741239631581573919339705649004208586949422810873621196157474272177586468236634536851618181572350294408509526514361027546939234421045026063811415872877733865949984217287267027217419")));
-	}
+//	public static void main(String[] args) {
+//		StreamCipher s = new StreamCipher(new BigInteger("4"), new BigInteger("11"), new BigInteger("3"), new BigInteger("5"));
+//		s.decrypt("fypid2xjcm9nWGFwKjsnXWpzKk5meSQ5NSU4Njw5ND01MygqNTkyNioxNzExKi0nYGYkMDY+ODw9OD05MDI6MzU2PDo5PCUgb25cdHR2KjsnPDs+MjY+ODQ6NTU5MDAyMT85JiQjcWx6cig5JUdrZ2UlZG10ZGppZyUo45ysICoocGh1dmtkJzMgOmsjb3JhbjwnYXZyejkoL3B/aHF9Z3QkYGhtK2xucmduaWtnKGFqbHNqYGYkKnFibDkqb2pvbWpmbHAiOlx2bH12Y3gjYW92KEBrbXBpY2c7L2U2IykrdnR/bWRhcG1lJzNkZ2ZwYiwmYW9ae2d2ZnpYdGtXcnFodnN5XG5kJjJvcGVuKihqaV92bXFpcF1yZVx0dGV8dHZWa2JVcHNyJjJvcGVuKihqaV92bXFpcF1yZVxyc2F6XmxtIDxkdmtsKCpoa1ZwY3pvfl9wZ15wemd0VWpjX3d8cyczbHNmbysibWZed2xyanNcc29be2J3bGdoVW1mbWEqO2t8bmomIXJzYXojP3Igb24hPTc3PzMyODExJiFuZFt7dXcrOCQ9MDAyMzkyMisuJGRiamUmMiNEZGBjeCNFcnFmcueVgCQmIXRjdm1ka1ZsZ2dmJTomS25pZnd0W3ZiZWpJQyclIGplYGZ0bWdvJzMgVUtNJ0FKXE5LQE0qKldCWEVbIykrd3RmIT1ucWRtKStmY3lgdWl0fGhqZyA8KEBITEtdUyVYd2NvbSdCKigiTWhrdCpQc3loYXJxKWtoKndvZSRFYG5gbGEkI04gSEdXQClBSUZMVSEkSnglSGx/Kk5iYWp7LyVIbH9+a25uYyhIdilSaXlwbmJobS8nJSB2eGxzZWd8ZGErOGBrb3RlKCp3YHtrYGNmYyI+bmBpemcqKGVobGhndmB7cVlpbHJucCo7Njg7KihldWlhZmV2VmFpf21zIj48NTclIGpjcHNlYFdianxscig5NjIoKmdkf21zeGpzZXdXYmp8bHIoOTQ3PCQjdn1jcn9wYnNba25wZ3YkMDY1NDMkI2Z7Z2d+ZmNfZXwjPytRZ34jVGV0KDA3KTA2MDE+OjQ/IS45MjY6IzUwNDEjKSt3cmlcaGZie2RxKzgrOzs3MDQkI3Fgb2NVeWhuYSo7J0pnaH5xZmwkXGhobCIuX1AnJiRLYGtoZmcjISsiY21uWmxsZ2hvYmQmMnV3fGcqKG9mbmMqOydsbCQmIWRvanxzbGt3cmVxdF9hZmBnZWdiKDlhYWh7ZCkra3VVd3VhanttZH1tdCg5YWFoe2QpK3J0ZWVubGFXY2RqaWF4bHJuYFdiamVtdCg5JTA0ODE1OSAqKHN1b2JhbWBWYGdpaGBya31vYVZra2tkYl9xem0nMyBufnd3OisnYGd6LHJ9ampnKmtuaCZra2tkYnMrfGlgZGd1JXdvZWltMDEmYGEkZG5mJiQjdXttYGNvYl9maWJubnBpf21jX21lYGJsXXN4b1hocHxxdis4JGJ3c3B3Mi4qaGB1JHdwaWlvL2ZmbyljbmZnYXsucWFna29wKHRsbWxgODYpaGQpZ21uIykrcnRlZW5sYVdjZGppYXhscm5gV3VsZWckMGVmbHdtLSd5cGlsamtlW2Roa2JdZWVvaHImMiM1SzY2PjMlLCZ4c2pva2pvXHRpYG1jZHtdZGVxY2V2V2JqZW10KDklMDQ4MTU5ICooc3VvYmFtYFZxb25mZWF2V2dsZW5ZaWxrb3YqOyc5MjY6MzciKCpxd2Zkb2ZmWHRhcHVaam1qZXElOiY4MTU5MjYoLyVwdmdnbGVnWX9wYl9maWJubnBpf21jX21lYGJsIDxsYmtzYSQjdXttYGNvYl9tZWBibF1zeG8lOiZgdXF5OCklc2VzKnx2bGRlKGlsai90em5jYG5jVWpqYWNtcio9OzYzNjI5Njw3PDo7Pz82NTArfVg8OUlCYjNYbmt6bGRlLGx6ZmAiKCpxd2Zkb2ZmWGlpaWZgVnd0ZlxvdHB4ciczIG5+d3dzPicudWtxKH50bm1jJmJqZC12eGxhaWhtXmxkY2FvcCg0PTg4MDw7ND41PjM9MTQwOzIpf1o+ME9MaTVWbGl4bmZsKmJxYG4gKihzdW9iYW1gVmBnZG1iclt9c2krOCRid3NwdzIuKnlgdSR3cGlpby9mZm8penFoZm1kZFprY2hkZnVzKz8yMjs1Nzk0KDE3MDc3PzM/PzElLCZsZGNod2p+XHdya25oaWwgPGxia3NhJCNhbGRnf29zX3R6bmNgbmNVamphY20jP29janlmKyJiZ21pZnVvZGQlOmp9bWklIGBlb2tvc1dzYHh3Y3l3WHNhZnUnM2xzZm8rImpndWxva2Vrd25vansjP2d3amZ+KyJjbW4nM2xzZm8rImdnbndta2hrd2JzJjJvcGVuKihza2FnbSM/ciBvbiE9IjdsZzFvNjQ9YTJhMjhnYGggKih2dWwmMiNtfXZ2eTkoL2V4aCt9dW9+d2JyKmtuaCYzKDssYGVrJ2hhJjFibDdhNDY/YzBoNDZsZmYubntuaysuJHpvZmNhV3V8eWckMCFkaXBxIykrbGdnZiU6JltgaylDaH5saWlrKi0nb3dqZlxpYWltIz8rUWdkI0ZucGdvbGYuJl5bJSwma25wZ3Z0c1xkb2BtIz8rV1UoLyVja31vcXt7JDAhUm5tfGRhKVFya3dicyYkI2dmd2huamlnW2pufSs4fSh3fnBhKjsnWW1qc2RobiYkI2ZmbXRuamlhcG1yJzNZXVEuPjgqPzY9PDc/JjE+LjU8MDw8NFsmWCo5PCY2MjE3MzMvNTkqPjg2OTYwVy9cLT0wLzY5MDE+NysyPSY3PDoyMjxeK1spMTkrOjI0PTczLDYxLzQ9Mz8/NVpdWXUtJ2h2cnhqZXVwbXInM3l7dy8lY2tmdXdgYHN+bHVzJjJvcGVuKihxYnRzbWRxVmFpf21zIj44LSdvY3BlcW50YVdianxscig5NywmbW9xYHZvb3AlOn8qaWR6anJrZHQiPlNcKSt2dG9tY3MmMlpYJSBzeG90Ij5TXCkrd3VvcVhtYWZ1bGZsdSg5XF0oKnJ8ZGBpZnAlOl9VfCkrZGd8bHVpcG1lJzNkZ2ZwYiwmemRxfmdjfmZjIj5uYGl6Zyooc2hzd2FjaXBddW9tdGlwYXdgKzhga290ZSgqZ2xldmN4XGtlcm1tJzMgamV0JSwmZGBrbiA8KGZpIigqdWxkZ3V+YmpwW2VyJzMgNz4wNjY1PDMyPjo0PCF6");
+//	}
+	
 }
